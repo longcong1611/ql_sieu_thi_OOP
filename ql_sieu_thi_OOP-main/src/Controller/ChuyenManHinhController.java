3b10903228c7740f8f471a4eedefac1f44b3179a
longcong1611 <142420624+longcong1611@users.noreply.github.com>
2025-04-28 16:26:18 +0700

    Add files via upload

---
+++ ql_sieu_thi_OOP-main/src/Controller/ChuyenManHinhController.java
@@ -0,0 +1,112 @@
+
+package Controller;
+
+import Bean.DanhMucBean;
+import View.SanPhamJPanel;
+import View.TrangChuJPanel;
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.util.List;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import org.w3c.dom.Node;
+
+public class ChuyenManHinhController {
+    private JPanel root;
+    private String kindSelected = "";
+    private List<DanhMucBean> listItem = null; 
+
+    public ChuyenManHinhController(JPanel jpnroot) {
+        this.root = jpnroot;
+    }
+    
+    public void setView(JPanel jpnItem, JLabel jlbItem){
+        kindSelected = "Trang chu";
+        jpnItem.setBackground(Color.green);
+        jlbItem.setBackground(Color.green);
+        root.removeAll();
+        root.setLayout(new BorderLayout());
+        root.add(new TrangChuJPanel());
+        root.validate();
+        root.repaint();
+    }
+    public void setEvent(List<DanhMucBean> listItem){
+        this.listItem = listItem;
+        for(DanhMucBean Item : listItem){ 
+            Item.getJlb().addMouseListener(new LabelEvent(Item.getKind(), Item.getJpn(), Item.getJlb()));
+        }       
+    }       
+     class LabelEvent implements MouseListener{
+         private JPanel node ;
+         private String kind ;
+         
+         private JPanel jpnItem;
+         private JLabel jlbItem;
+
+        public LabelEvent(String kind, JPanel jpnItem, JLabel jlbItem) {
+            this.kind = kind;
+            this.jpnItem = jpnItem;
+            this.jlbItem = jlbItem;
+        }
+        
+        @Override
+        public void mouseClicked(MouseEvent e) {
+            switch (kind) {
+                case "Trangchu":
+                    node = new TrangChuJPanel();
+                    break;
+                case "Sanpham":
+                    node = new SanPhamJPanel();
+                    break;
+                    default:
+                     node = new TrangChuJPanel();   
+                        break;
+            }
+            root.removeAll();
+            root.setLayout(new BorderLayout());
+            root.add(node);
+            root.validate();
+            root.repaint();
+            setChangeBackground(kind);
+        }
+
+        @Override
+        public void mousePressed(MouseEvent e) {
+            kindSelected = kind;
+            jpnItem.setBackground(Color.GREEN);
+            jlbItem.setBackground(Color.GREEN);
+        }
+
+        @Override
+        public void mouseReleased(MouseEvent e) {
+        }
+
+        @Override
+        public void mouseEntered(MouseEvent e) {
+            jpnItem.setBackground(Color.GREEN);
+            jlbItem.setBackground(Color.GREEN);
+        }
+
+        @Override
+        public void mouseExited(MouseEvent e){ 
+            if(kindSelected.equalsIgnoreCase(kind)){
+                jpnItem.setBackground(Color.WHITE);
+                jlbItem.setBackground(Color.WHITE);
+        }
+        } 
+            private void setChangeBackground(String kind){
+                for(DanhMucBean item : listItem ){
+                    if(item.getKind().equalsIgnoreCase(kind)){
+                        item.getJpn().setBackground(Color.GREEN);
+                        item.getJlb().setBackground(Color.GREEN);
+                }else{
+                        item.getJpn().setBackground(Color.WHITE);
+                        item.getJlb().setBackground(Color.WHITE);
+                    }
+            }
+     }
+
+}
+}
